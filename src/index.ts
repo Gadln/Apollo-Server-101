f5787c0183966604b890b787fed367e818b689dd
Gadln <helene.gadessaud@icloud.com>
2024-10-07 20:53:55 +0200

    Write a simple GraphQL server with Apollo Server

--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,143 @@
-import { ApolloServer } from '@apollo/server'; // preserve-line
-import { startStandaloneServer } from '@apollo/server/standalone'; // preserve-line
-// import express from "express";
-// import { createHandler } from "graphql-http/lib/use/express";
-// import { buildSchema } from "graphql";
+import "reflect-metadata";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import {
+  Arg,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+  buildSchema,
+} from "type-graphql";
+
+
+@ObjectType()
+class Book {
+    @Field()
+    id: string
+    @Field()
+    title: string
+    @Field()
+    author: string
+}
 
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+// const typeDefs = `#graphql
+//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    id: ID,
-    title: String
-    author: String
-  }
+//   # This "Book" type defines the queryable fields for every book in our data source.
+//   type Book {
+//     id: ID,
+//     title: String
+//     author: String
+//   }
 
-  type Mutation {
-  addBook(title: String, author: String): Book
-}
+//   type Mutation {
+//   addBook(title: String, author: String): Book
+// }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-    getBookById(bookId: ID): Book
-  }
-`;
+//   # The "Query" type is special: it lists all of the available queries that
+//   # clients can execute, along with the return type for each. In this
+//   # case, the "books" query returns an array of zero or more Books (defined above).
+//   type Query {
+//     books: [Book]
+//     getBookById(bookId: ID): Book
+//   }
+// `;
 
-const books = [
+const books: Book[] = [
   {
-    id: "0",
-    title: 'The Awakening',
-    author: 'Kate Chopin',
+    title: "The Awakening",
+    author: "Kate Chopin",
+    id: "1",
   },
   {
-    id: "1",
-    title: 'City of Glass',
-    author: 'Paul Auster',
+    title: "City of Glass",
+    author: "Paul Auster",
+    id: "2",
   },
 ];
 
+
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
-const resolvers = {
-  Query: {
-    books: () => books,
-    getBookById: (_: any, args: any) => books.find((book) => book.id == args.bookId)
-  },
-  Mutation: {
-    addBook: (_: any, args: any) => {
-      const lastBook = books.at(-1);
-      const lastId = lastBook ? parseInt(lastBook.id, 10) : 0;
-      const newId = (lastId + 1).toString();
-      books.push({
-        title: args.title,
-        author: args.author,
-        id: newId,
-      });
-      return books.at(-1);
-    },
-  },
-};
+// const resolvers = {
+//   Query: {
+//     books: () => books,
+//     getBookById: (_: any, args: any) => books.find((book) => book.id == args.bookId)
+//   },
+//   Mutation: {
+//     addBook: (_: any, args: any) => {
+//       const lastBook = books.at(-1);
+//       const lastId = lastBook ? parseInt(lastBook.id, 10) : 0;
+//       const newId = (lastId + 1).toString();
+//       books.push({
+//         title: args.title,
+//         author: args.author,
+//         id: newId,
+//       });
+//       return books.at(-1);
+//     },
+//   },
+// };
+
+@InputType()
+class BookInput {
+  @Field()
+  title: string;
+
+  @Field()
+  author: string;
+}
+
+@Resolver(Book)
+class BookResolver {
+  @Query(() => [Book])
+  books() {
+    return books;
+  }
+
+  @Query(() => Book)
+  getBookById(@Arg("id") id: string) {
+    return books.find((book) => book.id == id);
+  }
+
+  @Mutation(() => Book)
+  addBook(@Arg("data") { title, author }: BookInput) {
+    const lastBook = books.at(-1);
+    const lastId = lastBook ? parseInt(lastBook.id, 10) : 0;
+    const id = (lastId + 1).toString();
+    books.push({ title, author, id });
+    return books.at(-1);
+  }
+}
+
 
 // // The ApolloServer constructor requires two parameters: your schema
 // // definition and your set of resolvers.
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+
 
 // // Passing an ApolloServer instance to the `startStandaloneServer` function:
 // //  1. creates an Express app
 // //  2. installs your ApolloServer instance as middleware
 // //  3. prepares your app to handle incoming requests
 (async () => {
+
+  const schema = await buildSchema({
+    resolvers: [BookResolver],
+  });
+  
+  const server = new ApolloServer({ schema });
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
   });
@@ -93,31 +153,3 @@ const server = new ApolloServer({
 
 
 
-// Construct a schema, using GraphQL schema language
-// var schema = buildSchema(`
-//   type Query {
-//     hello: String
-//   }
-// `)
- 
-// The root provides a resolver function for each API endpoint
-// var root = {
-//   hello() {
-//     return "Hello world!"
-//   },
-// }
- 
-//var app = express()
- 
-// Create and use the GraphQL handler.
-// app.all(
-//   "/graphql",
-//   createHandler({
-//     schema: schema,
-//     rootValue: root,
-//   })
-// )
- 
-// Start the server at port
-// app.listen(4000)
-// console.log("Running a GraphQL API server at http://localhost:4000/graphql")
