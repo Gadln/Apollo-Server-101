29e20ac8adaba48d41ab445716b2fa76501ccbaa
Gadln <helene.gadessaud@icloud.com>
2024-10-07 20:55:06 +0200

    Clean code

--- src/index.ts
+++ src/index.ts
@@ -23,33 +23,6 @@ class Book {
     author: string
 }
 
-
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-// const typeDefs = `#graphql
-//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-//   # This "Book" type defines the queryable fields for every book in our data source.
-//   type Book {
-//     id: ID,
-//     title: String
-//     author: String
-//   }
-
-//   type Mutation {
-//   addBook(title: String, author: String): Book
-// }
-
-//   # The "Query" type is special: it lists all of the available queries that
-//   # clients can execute, along with the return type for each. In this
-//   # case, the "books" query returns an array of zero or more Books (defined above).
-//   type Query {
-//     books: [Book]
-//     getBookById(bookId: ID): Book
-//   }
-// `;
-
 const books: Book[] = [
   {
     title: "The Awakening",
@@ -62,29 +35,6 @@ const books: Book[] = [
     id: "2",
   },
 ];
-
-
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
-// const resolvers = {
-//   Query: {
-//     books: () => books,
-//     getBookById: (_: any, args: any) => books.find((book) => book.id == args.bookId)
-//   },
-//   Mutation: {
-//     addBook: (_: any, args: any) => {
-//       const lastBook = books.at(-1);
-//       const lastId = lastBook ? parseInt(lastBook.id, 10) : 0;
-//       const newId = (lastId + 1).toString();
-//       books.push({
-//         title: args.title,
-//         author: args.author,
-//         id: newId,
-//       });
-//       return books.at(-1);
-//     },
-//   },
-// };
 
 @InputType()
 class BookInput {
@@ -117,20 +67,6 @@ class BookResolver {
   }
 }
 
-
-// // The ApolloServer constructor requires two parameters: your schema
-// // definition and your set of resolvers.
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-// });
-
-
-
-// // Passing an ApolloServer instance to the `startStandaloneServer` function:
-// //  1. creates an Express app
-// //  2. installs your ApolloServer instance as middleware
-// //  3. prepares your app to handle incoming requests
 (async () => {
 
   const schema = await buildSchema({
